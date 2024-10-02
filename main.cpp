87daa07bb5d1234e917cc38bafe82852161e12d6
joshsellers <joshsellers7.5@gmail.com>
2024-10-02 02:12:13 -0500

    Add some new functions

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "Program.h"
 #include "ModManager.h"
 
-const std::string VERSION = "1.10";
+const std::string VERSION = "1.11";
 
 int main(int argc, char* argv[]) {
     std::cout << "bygro v" + VERSION << std::endl << std::endl;
